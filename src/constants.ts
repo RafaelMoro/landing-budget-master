da8b947be66228788c03457e72cc7e89213f8548
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-12-13 14:59:52 -0600

    add dashboard route

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 // TODO: Cambiar url por la buena
-export const BUDGET_MASTER_APP_URL = 'https://budget-master.app.space/'
+export const BUDGET_MASTER_APP_URL = 'https://budget-master.app.space'
 
 export const BUDGET_MASTER_LOGIN_ROUTE = `${BUDGET_MASTER_APP_URL}/login`
 export const BUDGET_MASTER_REGISTER_ROUTE = `${BUDGET_MASTER_APP_URL}/register`
+export const BUDGET_MASTER_DASHBOARD_ROUTE = `${BUDGET_MASTER_APP_URL}/dashboard`
