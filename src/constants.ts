3e370edebb6d16538928f6e7bb3f41aabadf60e6
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-12-24 16:29:41 -0600

    add constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,5 @@ export const BUDGET_MASTER_APP_URL = 'https://budget-master.app.space'
 export const BUDGET_MASTER_LOGIN_ROUTE = `${BUDGET_MASTER_APP_URL}/login`
 export const BUDGET_MASTER_REGISTER_ROUTE = `${BUDGET_MASTER_APP_URL}/register`
 export const BUDGET_MASTER_DASHBOARD_ROUTE = `${BUDGET_MASTER_APP_URL}/dashboard`
+export const BUDGET_MASTER_EMAIL = 'sugerencias-quejas@budget-master.space'
+export const BUDGET_MASTER_SEND_EMAIL = `mailto:${BUDGET_MASTER_EMAIL}`
