9d64366b2dd16b9751a4212e7460c38104033ea5
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-01-08 23:12:32 -0600

    fix budget master app login route

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const BUDGET_MASTER_APP_URL = 'https://app.budget-master.space'
 
-export const BUDGET_MASTER_LOGIN_ROUTE = `${BUDGET_MASTER_APP_URL}/login`
+export const BUDGET_MASTER_LOGIN_ROUTE = `${BUDGET_MASTER_APP_URL}`
 export const BUDGET_MASTER_REGISTER_ROUTE = `${BUDGET_MASTER_APP_URL}/register`
 export const BUDGET_MASTER_DASHBOARD_ROUTE = `${BUDGET_MASTER_APP_URL}/dashboard`
 export const BUDGET_MASTER_EMAIL = 'sugerencias-quejas@budget-master.space'
