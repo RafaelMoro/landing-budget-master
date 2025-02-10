34330815e2da9af4aac53f8359ae69ae4b2f2fc7
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-10 08:40:54 -0600

    style content blog

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@ export const BUDGET_MASTER_LANDING_URL = 'https://budget-master.space'
 export const BUDGET_MASTER_LOGIN_ROUTE = `${BUDGET_MASTER_APP_URL}`
 export const BUDGET_MASTER_REGISTER_ROUTE = `${BUDGET_MASTER_APP_URL}/register`
 export const BUDGET_MASTER_DASHBOARD_ROUTE = `${BUDGET_MASTER_APP_URL}/dashboard`
+export const BUDGET_MASTER_TRY_APP_ROUTE = `${BUDGET_MASTER_APP_URL}/try-app`
 export const BUDGET_MASTER_SUPPORT_EMAIL = 'soporte@budget-master.space'
 export const BUDGET_MASTER_SEND_EMAIL_SUPPORT = `mailto:${BUDGET_MASTER_SUPPORT_EMAIL}`
 
