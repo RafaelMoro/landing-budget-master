d62cca38ea069ba611c88ea0f1da8d9ecbf91c21
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-02-13 01:32:44 -0600

    add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import partytown from '@astrojs/partytown';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), partytown({
     config: {
       forward: ['dataLayer.push']
     }
-  })]
+  }), sitemap()]
 });
