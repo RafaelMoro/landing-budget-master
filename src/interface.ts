d8571a5389f261735ac920ab677963d33467957b
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-03-06 18:33:13 -0600

    update interfaces of subcontent and subcontent extended

--- src/interface.ts
+++ src/interface.ts
@@ -1,9 +1,21 @@
-export interface BasicTableContents {
+export interface Subcontent {
   contentId: string;
   anchorId: string;
   text: string;
 }
 
-export interface ExtendedTableContents extends BasicTableContents {
+export interface SubcontentExtended extends Subcontent {
   href: string;
+}
+
+export interface BasicTableContents {
+  contentId: string;
+  anchorId: string;
+  text: string;
+  subcontent?: Subcontent[];
+}
+
+export interface ExtendedTableContents extends Omit<BasicTableContents, 'subcontent'> {
+  href: string;
+  subcontent?: SubcontentExtended[];
 }
