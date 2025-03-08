dafb8957090c4184068b3a50bee6ee5e16467f8e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-03-08 13:03:17 -0600

    add table contents cashback  blogpost

---
+++ src/pages/blog/que-es-cashback/cashback.content.ts
@@ -0,0 +1,27 @@
+import { BLOG_BEST_JOBS_PAID_MEXICO_ROUTE } from "@constants";
+import { getExtendedTableContents } from "@helpers/tableContents.helper";
+import type { BasicTableContents } from "@interface";
+
+export const tableContentsBasic: BasicTableContents[] = [
+  {
+    contentId: "cashback-intro",
+    anchorId: "title-cashback-intro",
+    text: "Cashback: qué es y cómo obtener reembolsos en cada compra",
+  },
+  {
+    contentId: "history-cashback",
+    anchorId: "title-history-cashback",
+    text: "Del pasado al presente: Cómo evolucionó el cashback",
+  },
+  {
+    contentId: "cashback-types",
+    anchorId: "title-cashback-types",
+    text: "Tipos de cashback",
+  },
+  { contentId: "conclusion", anchorId: "title-conclusion", text: "Conclusión" },
+];
+
+export const extendedTableContents = getExtendedTableContents({
+  tableContents: tableContentsBasic,
+  route: BLOG_BEST_JOBS_PAID_MEXICO_ROUTE,
+});
